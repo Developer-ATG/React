35b023ed790a41f11d88acf7354872faa0dd8753
DeveloperATG <amey.ghule111@gmail.com>
2022-01-05 22:32:30 +0530

    portal
    portal practice

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import AddUser from './components/User/AddUser';
 import UsersList from './components/User/UsersList';
 
@@ -14,10 +14,10 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
---
+++ demo/src/components/Helpers/Wrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Wrapper = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Wrapper
