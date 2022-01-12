5b3c47486241622d392f2680ebf088a60e21453b
DeveloperATG <amey.ghule111@gmail.com>
2022-01-12 22:04:46 +0530

    simple change

--- food_app/src/components/Meals/MealItem/MealItemForm.js
+++ food_app/src/components/Meals/MealItem/MealItemForm.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
             <Input
                 label='Amount'
                 input={{
-                    id: 'amount',
+                    id: 'amount_' + props.id,
                     type: 'number',
                     min: '1',
                     max: '5',
