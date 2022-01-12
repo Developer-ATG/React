919f629d01235206c961eb66ce69ae0926f80a7a
DeveloperATG <amey.ghule111@gmail.com>
2022-01-12 22:01:17 +0530

    form creation

--- food_app/src/components/Meals/MealItem/MealItem.js
+++ food_app/src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -12,7 +14,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm></MealItemForm>
             </div>
         </li>
     )
