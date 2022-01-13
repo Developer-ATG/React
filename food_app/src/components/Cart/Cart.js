e5b5c34d88f00dace095fdc170bf49e66fd6c035
DeveloperATG <amey.ghule111@gmail.com>
2022-01-13 12:18:42 +0530

    git ignore changes

---
+++ food_app/src/components/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Cart.module.css'
+
+const Cart = () => {
+    return (
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+
+    )
+}
+
+export default Cart
