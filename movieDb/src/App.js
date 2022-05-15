206a9123a7d180b8c5df896cdf0337648b2fd159
DeveloperATG <amey.ghule111@gmail.com>
2022-05-15 16:22:59 +0530

    Header updation

--- movieDb/src/App.js
+++ movieDb/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>Welcome</div>
-  )
-}
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" exact element={Home} />
+          <Route path="/movie/:imdbID" element={MovieDetail} />
+          <Route element={PageNotFound} />
+        </Routes>
+        <Footer></Footer>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- movieDb/src/components/Header/Header.js
+++ movieDb/src/components/Header/Header.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../../images/Sample_User_Icon.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <Link to={"/"}>
+		<div className="logo" alt="Movie App"></div>
+      </Link>
+      <div className="user-img">
+        <img src={user} alt="user" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
